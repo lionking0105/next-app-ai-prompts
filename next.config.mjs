3dae8cc501bb91b66cd00e2f114c794abd89724b
lionking0105 <alexanderfoster0105@gmail.com>
2024-02-11 23:44:23 +0200

    updated nextConfig: fixed images.domains is deprecated; added remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,13 @@ const nextConfig = {
     serverComponentsExternalPackages: ["mongoose"],
   },
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '**',
+      },
+    ]
   },
   webpack(config) {
     config.experiments = {
