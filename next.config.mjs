d5ef9fe14042935e7247ed9bf37f89f499b72a9b
lionking0105 <alexanderfoster0105@gmail.com>
2024-02-11 22:34:51 +0200

    updated nextConfig: added serverComponentsExternalPackages & images & webpack & headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config
+  },
+  headers: [
+    {
+      key: "Cache-Control",
+      value: "private, no-cache, no-store, max-age=0, must-revalidate",
+    },
+  ]
+}
 
 export default nextConfig;
