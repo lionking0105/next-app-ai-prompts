d94c044315612f3c8e2c7a67c019073d5101da93
lionking0105 <alexanderfoster0105@gmail.com>
2024-02-11 22:12:58 +0200

    created database.js & added connectToDatabase with mongoose

---
+++ utils/database.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+let isConnected = false; // track the connection
+
+export const connectToDatabase = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("Mongo DB is already connected");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = true;
+    console.log("✔ MongoDB successfully conected");
+  } catch (error) {
+    console.log(error);
+  }
+}
