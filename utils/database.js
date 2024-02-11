5eec7ad98d895314b9f0e76c1088e32e16984f11
lionking0105 <alexanderfoster0105@gmail.com>
2024-02-12 00:30:03 +0200

    updated database.js: deleted deprecated options - useNewUrlParser & useUnifiedTopology

--- utils/database.js
+++ utils/database.js
@@ -12,8 +12,6 @@ export const connectToDatabase = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "share_prompt",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
 
     isConnected = true;
