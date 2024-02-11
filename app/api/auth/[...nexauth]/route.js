9ee32dc03d4dacdcd75806f48d84990b1ec32261
lionking0105 <alexanderfoster0105@gmail.com>
2024-02-11 22:17:02 +0200

    added NextAuth; created handler with GoogleProvider; added session & signIn

--- app/api/auth/[...nexauth]/route.js
+++ app/api/auth/[...nexauth]/route.js
@@ -1,18 +1,48 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDatabase } from "@/utils/database";
+import User from "@/models/user";
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clienId: "",
-      clientSecret: ""
+      clienId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
   async session({ session }) {
+    // store the user id from MongoDB to session
+    const sessionUser = await User.findOne({
+      email: session.user.email
+    });
+    session.user.id = sessionUser._id.toString();
 
+    return session;
   },
   async signIn({ profile }) {
+    try {
+      // serverless -> lambda
+      await connectToDatabase();
 
+      // check if user already exists
+      const userExists = await User.findOne({
+        email: profile.email
+      });
+
+      // if not, create a new document and save user in MongoDB
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log("Error checking if user exists: ", error.message);
+      return false;
+    }
   }
 });
 
