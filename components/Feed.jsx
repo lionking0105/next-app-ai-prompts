c76e2a36969b1e88ccd7d47a3cf30548ef5d5604
lionking0105 <alexanderfoster0105@gmail.com>
2024-02-12 14:47:29 +0200

    updated Feed component: added fetchPosts function to get the data back as allPosts

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,6 +1,46 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { PromptCardList } from ".";
+
 const Feed = () => {
+  const [allPosts, setAllPosts] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setAllPosts(data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, [])
+
+  const handleSearchChange = (event) => {
+
+  }
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          className="search_input peer"
+          type="text"
+          placeholder="Search for a prompt by tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+        />
+      </form>
+
+      {/* Show All prompts */}
+      <PromptCardList
+        data={allPosts}
+        handleTagClick={() => { }}
+      />
+    </section>
   )
 }
 
