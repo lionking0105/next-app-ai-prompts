5c649aae0de21ffc2f828686d5ac805f7e54e3ab
lionking0105 <alexanderfoster0105@gmail.com>
2024-02-12 21:12:47 +0200

    Added functionality to the Profile component, which shows all the Prompts of the current user with the ability to edit or delete a specific Promt; also added a copy button in the PromptCard component

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,6 +1,30 @@
-const Profile = () => {
+import { PromptCard } from "."
+
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
-    <div>Profile</div>
+    <section className="w-full">
+      {name === "My" ? (
+        <h1 className="head_text text-left">
+          <span className="green_gradient">{name} profile</span>
+        </h1>
+      ) : (
+        <h1 className="head_text text-left">
+          Profile: <span className="green_gradient">{name}</span>
+        </h1>
+      )}
+      <p className="desc text-left">{desc}</p>
+
+      <div className="mt-10 prompt_layout">
+        {data.map(post => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleEdit={() => handleEdit && handleEdit(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
+          />
+        ))}
+      </div>
+    </section>
   )
 }
 
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,6 +6,16 @@ import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const [copied, setCopied] = useState("");
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(""), 6000);
+  }
+
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
@@ -21,9 +31,36 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             <h3 className="font-satoshi font-semibold text-green-700">{post.creator.username}</h3>
           </div>
         </div>
+        <button className="copy_btn"
+          onClick={handleCopy}
+          aria-label="copy buton"
+        >
+          <Image
+            src={copied === post.prompt
+              ? "/assets/icons/tick.svg"
+              : "/assets/icons/copy.svg"
+            }
+            alt={copied === post.prompt
+              ? "tick icon"
+              : "copy icon"
+            }
+            width={16}
+            height={16}
+          />
+        </button>
       </div>
       <p className="my-5 font-satoshi text-sm text-gray-700">{post.prompt}</p>
-      <p className="font-inter text-sm blue_gradient cursor-pointe">#{post.tag}</p>
+      <p className="font-inter text-sm blue_gradient cursor-pointe"
+        onClick={() => handleClick && handleTagClick(post.tag)}>#{post.tag}</p>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 pt-3 flex-center gap-4 border-t border-gray-100">
+          <button onClick={handleEdit} aria-label="edit button"
+            className="font-inter text-md green_gradient">Edit</button>
+          <button onClick={handleDelete} aria-label="delete button"
+            className="font-inter text-md orange_gradient">Delete</button>
+        </div>
+      )}
     </div>
   )
 }
